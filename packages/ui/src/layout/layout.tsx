efd7dc37e7d8830632805fac8ea802e0ca849afb
Kalle <kh.hansson@gmail.com>
2020-03-20 07:34:38 +0100

    Added some ui content etc

---
+++ packages/ui/src/layout/layout.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled';
+
+import { Layout as AntLayout } from 'antd';
+
+export const Layout = styled(AntLayout)`
+	height: 100%;
+`;
+
+export const Content = styled(AntLayout.Content)`
+	padding: 24px;
+`;
+
+export const Header = styled(AntLayout.Header)`
+	color: white;
+`;
---
+++ shared/styled/index.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export { styled };
+
+export * from 'styled-components';
+
+export default styled;
